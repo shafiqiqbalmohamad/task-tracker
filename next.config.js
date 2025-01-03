a5fc657df74fc5c2fc0fd8c83bc5853a80294f17
shafiqiqbalmohamad <shafiq.iqbal.c940@gmail.com>
2025-01-03 21:41:10 +0800

    first updating

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  images: {
+    unoptimized: true,
+    domains: ["https://rvnzumxpayqhvspangrb.supabase.co"],
+  },
 };
 
 module.exports = nextConfig;
